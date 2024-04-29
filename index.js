e9d4a55ab05090af526370d2e841ec8ff421ee4f
Carlos García <67180099+CarlosG4rc@users.noreply.github.com>
2024-04-28 19:36:53 -0600

    adding last lines

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from 'express'; // Importo express
+import bodyParser from 'body-parser'; // Importo el body-parser
 const app = express();
-import userRouter from './routes/users.js';
+import userRouter from './routes/users.js'; // Importo el archivo de rutas
 
 const PORT = 5000;
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // Middleware para parsear el body a JSON
 
-app.use('/users', userRouter);
+app.use('/users', userRouter); // Middleware para las rutas de /users
 
-app.get('/', (req, res) => res.send('Hola mundo!'));
+app.get('/', (req, res) => res.send('Hola mundo!')); // Ruta de prueba 
 
-app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`)); // Inicio el servidor
